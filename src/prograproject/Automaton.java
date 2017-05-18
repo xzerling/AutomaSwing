da9a5a4c4e8e9e852258a5d47c7f41fc22c19f4b
Rocker2503 <npradenas.vargas@gmail.com>
2017-05-18 18:38:50 -0400

    separacion clase transition

--- src/prograproject/Automaton.java
+++ src/prograproject/Automaton.java
@@ -45,31 +45,3 @@ public class Automaton
         return finState;
     }
 }
-
-class Transition
-{
-    private String start, end;
-    private char symbol; 
-    
-    public Transition(String s, String e, char sym)
-    {
-        this.start = s;
-        this.end = e;
-        this.symbol = sym;
-    }
-    
-    public String getStart()
-    {
-        return start;
-    }
-    
-    public String getEnd()
-    {
-        return end;
-    }
-    
-    public char getSymbol()
-    {
-        return symbol;
-    }
-}
---
+++ src/prograproject/Transition.java
@@ -0,0 +1,33 @@
+
+package prograproject;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Transition {
+    private String start, end;
+    private char symbol; 
+    
+    public Transition(String s, String e, char sym)
+    {
+        this.start = s;
+        this.end = e;
+        this.symbol = sym;
+    }
+    
+    public String getStart()
+    {
+        return start;
+    }
+    
+    public String getEnd()
+    {
+        return end;
+    }
+    
+    public char getSymbol()
+    {
+        return symbol;
+    }
+}
