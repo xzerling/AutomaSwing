22c8e0a856bb617a706fc526a5bcff8ecc61a53d
default <default@DevouringTablet>
2017-05-25 23:29:46 -0400

    trabajando en solapamiento nodo linea; -ecuacion recta y weas.

--- src/prograproject/JDrawPanel.java
+++ src/prograproject/JDrawPanel.java
@@ -466,7 +466,7 @@ public class JDrawPanel extends JPanel implements MouseMotionListener, MouseList
             System.out.println("x1: " + x1);
             System.out.println("y1: " + y1);
             System.out.println("x2: " + x2);
-            System.out.println("y2: " + y2);
+//            System.out.println("y2: " + y2);
             
             m =  (x2-(x1))/(y1-(y2));
             System.out.println("M: " + m);
